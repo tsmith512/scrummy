d0a641e6fc98839e9caa0b8e676cebd4f42f8f14
Taylor Smith <tsmith@tsmithcreative.com>
2013-01-29 15:26:51 -0600

    Update bucket on client departues so new clients get correct list

--- scrum_cards_server.js
+++ scrum_cards_server.js
@@ -13,6 +13,7 @@ server.listen(config.port);
 io.sockets.on('connection',function(socket){
 
   socket.on('signIn',function(data,fn){
+    console.log(data);
 
     for(sock in sockDict){
       var name = sockDict[sock];
@@ -22,8 +23,15 @@ io.sockets.on('connection',function(socket){
       }
     }
 
+    for ( client in bucket ) {
+      if (client.nickname == data.userName) {
+        fn( false, 'Nickname already in use.' );
+        return;
+      }
+    }
+
     sockDict[socket.id] = data.userName;
-    bucket[bucket.length] = {id: socket.id, nickname: data.userName};
+    bucket.push({id: socket.id, nickname: data.userName});
     socket.broadcast.emit('userSignedIn',{ 'userName' : data.userName});
     fn(true,{ 'points' : config.points, 'users' : bucket} );
   });
@@ -38,6 +46,17 @@ io.sockets.on('connection',function(socket){
   });
 
   socket.on('disconnect',function(){
+
+    /* Iterate over the bucket _backwards_ so we can cleanly remove the departing
+     * client having to recalculate the length (as you would in a for loop) */
+    var i = bucket.length;
+    while (i--) {
+      if (bucket[i].id == socket.id) {
+        bucket.splice(i, 1);
+      }
+    }
+
+
     socket.broadcast.emit('clientDisconnect', {userName : sockDict[socket.id] });
     sockDict[socket.id] = null;
   });
