65f7701bfadb4e49a11188812e3806d9d8f5d41d
Taylor Smith <tsmith@tsmithcreative.com>
2023-02-11 22:34:43 -0600

    Fix indentation; pass a sample game state object taken from scrum_cards_server.js

--- worker/src/ScrummyGame.ts
+++ worker/src/ScrummyGame.ts
@@ -1,13 +1,48 @@
-import { Env } from ".";
+import { Router } from "itty-router";
+import { basic404, Env } from ".";
+
+export interface Player {
+  nick: string;
+  vote: number;
+}
+
+export interface GameState {
+  name: string;
+  id: string;
+  reveal: boolean;
+  players: Player[];
+}
+
+const sampleState: GameState = {
+  name: 'Test Game',
+  id: 'unknown durable object id',
+  reveal: false,
+  players: [
+    { nick: 'Ted', vote: 5 },
+    { nick: 'Linda', vote: 5 },
+    { nick: 'Lem', vote: 13 },
+    { nick: 'Phil', vote: 8 },
+  ],
+}
 
 export class ScrummyGame {
-	state: DurableObjectState;
+  state: DurableObjectState;
 
-	constructor(state: DurableObjectState, env: Env) {
-		this.state = state;
-	}
+  constructor(state: DurableObjectState, env: Env) {
+    this.state = state;
+  }
 
-	async fetch(request: Request) {
-		return new Response(JSON.stringify(request.url));
-	}
+  async fetch(request: Request) {
+    const router = Router();
+
+    router.get('/status', async (request, env: Env, ctx) => {
+      return new Response(JSON.stringify(sampleState));
+    });
+
+    router.all('*', basic404);
+
+    console.log(JSON.stringify(request));
+
+    return await router.handle(request);
+  }
 }
--- worker/src/index.ts
+++ worker/src/index.ts
@@ -4,7 +4,7 @@ import { Router } from 'itty-router';
  * Environment variables and bindings to DO, R2, KV, etc.
  */
 export interface Env {
-	GAME: DurableObjectNamespace;
+  GAME: DurableObjectNamespace;
 }
 
 /**
@@ -23,11 +23,13 @@ const router = Router();
 // (_-</ -_)  _| || | '_ \
 // /__/\___|\__|\_,_| .__/
 //                 |_|
-const basic404 = () => new Response('Route not found', { status: 404 });
+export const basic404 = () => new Response('Route not found', { status: 404 });
+
 const basic200 = () => new Response('Scrummy backend is running', {
   status: 200,
   headers: globalheaders,
 });
+
 const basicCors = () => new Response(null, {
   status: 204,
   headers: globalheaders,
@@ -37,17 +39,17 @@ router.get('/api', basic200);
 router.options('*', basicCors);
 
 router.get('/api/:game', async (request, env: Env, context: any) => {
-	const name = request.params?.game || false;
+  const name = request.params?.game || false;
 
-	if (!name || name.match(/^[A-Za-z0-9-_]$/g)) {
-		return;
-	}
+  if (!name || name.match(/^[A-Za-z0-9-_]$/g)) {
+    return;
+  }
 
-	const id = env.GAME.idFromName(name);
-	const game = env.GAME.get(id);
-	const url = new URL(request.url);
+  const id = env.GAME.idFromName(name);
+  const game = env.GAME.get(id);
+  const url = new URL(request.url);
 
-	return await game.fetch(`${url.protocol}//${url.hostname}/status`);
+  return await game.fetch(`${url.protocol}//${url.hostname}/status`);
 });
 
 // Fallback: any request not already caught is a 404.
@@ -62,7 +64,7 @@ export default {
   // Inbound requests pass as-is to the router.
   fetch: router.handle,
 
-	// @TODO: scheduled jobs for garbage collecting?
+  // @TODO: scheduled jobs for garbage collecting?
 };
 
 export { ScrummyGame } from "./ScrummyGame";
