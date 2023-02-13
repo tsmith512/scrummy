9ebc6652368176982490c553901df37cd3178454
Taylor Smith <tsmith@tsmithcreative.com>
2023-02-13 00:38:30 -0600

    Fix HTTPS link to Google Fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href='http://fonts.googleapis.com/css?family=Fjalla+One|Stint+Ultra+Expanded|Open+Sans:400italic,400,700' rel='stylesheet' type='text/css' />
+        <link href='https://fonts.googleapis.com/css?family=Fjalla+One|Stint+Ultra+Expanded|Open+Sans:400italic,400,700' rel='stylesheet' type='text/css' />
       </Head>
       <body>
         <Main />
