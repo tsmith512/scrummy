f7ebb0217e3770719810b1df5a39d8d60bcd5731
Taylor Smith <tsmith@tsmithcreative.com>
2023-02-14 00:48:12 -0600

    Don't boot out 20 point voters anymore

--- worker/src/ScrummyGame.ts
+++ worker/src/ScrummyGame.ts
@@ -257,11 +257,6 @@ export class ScrummyGame {
       const player = await request.json() as Player;
       const success = this.playerUpdate(player);
 
-      // Test doing an unannounced kick of a user to see if this works
-      // client-side. Boot the user who sizes something a 20.
-      if (player.vote === 20) {
-        await this.playerRemove(player);
-      }
       return new Response(null, {
         status: (success) ? 202 : 400
       });
